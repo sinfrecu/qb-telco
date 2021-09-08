96f4eec23cdb6080cc64754735084701c8ac612a
matias <matias@matiasba.com.ar>
2021-09-08 06:03:24 +0000

    test job requeriment

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,6 @@
 isLoggedIn = false
 PlayerData = {}
+local PlayerJob = {}
 
 local BuilderData = {
     ShowDetails = false,
@@ -10,9 +11,21 @@ RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
 AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
     isLoggedIn = true
     PlayerData = QBCore.Functions.GetPlayerData()
+    PlayerJob = QBCore.Functions.GetPlayerData().job
     GetCurrentProject()
     TriggerEvent('qb-telco:client:UpdateBlip', Config.CurrentProject)
 end)
+
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate')
+AddEventHandler('QBCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerJob = JobInfo
+    TriggerEvent('qb-telco:client:UpdateBlip', Config.CurrentProject)
+end)
+
+
+
+
 
 Citizen.CreateThread(function()
     Wait(1000)
@@ -63,85 +76,87 @@ Citizen.CreateThread(function()
         local pos = GetEntityCoords(ped)
         local inRange = false
         local OffsetZ = 0.2
-        
-        if Config.CurrentProject ~= 0 then
-            local data = Config.Projects[Config.CurrentProject].ProjectLocations["main"]
-            local MainDistance = #(pos - vector3(data.coords.x, data.coords.y, data.coords.z))
-            
-            -- Main blip
-            if MainDistance < 10 then
-                inRange = true
-                DrawMarker(2, data.coords.x, data.coords.y, data.coords.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 0.2, 55, 155, 255, 255, 0, 0, 0, 1, 0, 0, 0)
+        if PlayerJob.name == "telco" then
+            if Config.CurrentProject ~= 0 then
+                local data = Config.Projects[Config.CurrentProject].ProjectLocations["main"]
+                local MainDistance = #(pos - vector3(data.coords.x, data.coords.y, data.coords.z))
 
-                if MainDistance < 2 then
-                    local TaskData = GetCompletedTasks()
-                    if TaskData ~= nil then
-                        if not BuilderData.ShowDetails then
-                            DrawText3Ds(data.coords.x, data.coords.y, data.coords.z, '[E] Detail view')
-                            DrawText3Ds(data.coords.x, data.coords.y, data.coords.z + 0.2, 'Exercises: '..TaskData.completed..' / '..TaskData.total)
-                        else
-                            DrawText3Ds(data.coords.x, data.coords.y, data.coords.z, '[E] Details hide')
-                            for k, v in pairs(Config.Projects[Config.CurrentProject].ProjectLocations["tasks"]) do
-                                if v.completed then
-                                    DrawText3Ds(data.coords.x, data.coords.y, data.coords.z + OffsetZ, v.label..': Completed')
-                                else
-                                    DrawText3Ds(data.coords.x, data.coords.y, data.coords.z + OffsetZ, v.label..': Not completed')
+                -- Main blip
+                if MainDistance < 10 then
+                    inRange = true
+                    DrawMarker(2, data.coords.x, data.coords.y, data.coords.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 0.2, 55, 155, 255, 255, 0, 0, 0, 1, 0, 0, 0)
+
+                    if MainDistance < 2 then
+                        local TaskData = GetCompletedTasks()
+                        if TaskData ~= nil then
+                            if not BuilderData.ShowDetails then
+                                DrawText3Ds(data.coords.x, data.coords.y, data.coords.z, '[E] Detail view')
+                                DrawText3Ds(data.coords.x, data.coords.y, data.coords.z + 0.2, 'Exercises: '..TaskData.completed..' / '..TaskData.total)
+                            else
+                                DrawText3Ds(data.coords.x, data.coords.y, data.coords.z, '[E] Details hide')
+                                for k, v in pairs(Config.Projects[Config.CurrentProject].ProjectLocations["tasks"]) do
+                                    if v.completed then
+                                        DrawText3Ds(data.coords.x, data.coords.y, data.coords.z + OffsetZ, v.label..': Completed')
+                                    else
+                                        DrawText3Ds(data.coords.x, data.coords.y, data.coords.z + OffsetZ, v.label..': Not completed')
+                                    end
+                                    OffsetZ = OffsetZ + 0.2
                                 end
-                                OffsetZ = OffsetZ + 0.2
                             end
-                        end
 
-                        if TaskData.completed == TaskData.total then
-                            DrawText3Ds(data.coords.x, data.coords.y, data.coords.z - 0.2, '[G] End project')
-                            if IsControlJustPressed(0, 47) then
-                                TriggerServerEvent('qb-telco:server:FinishProject')
+                            if TaskData.completed == TaskData.total then
+                                DrawText3Ds(data.coords.x, data.coords.y, data.coords.z - 0.2, '[G] End project')
+                                if IsControlJustPressed(0, 47) then
+                                    TriggerServerEvent('qb-telco:server:FinishProject')
+                                end
                             end
-                        end
-
-                        if IsControlJustPressed(0, 38) then
-                            BuilderData.ShowDetails = not BuilderData.ShowDetails
-                        end
-                    end
-                end
-            end
-
-            for k, v in pairs(Config.Projects[Config.CurrentProject].ProjectLocations["tasks"]) do
-                if not v.completed or not v.IsBusy then
-                    local TaskDistance = #(pos - vector3(v.coords.x, v.coords.y, v.coords.z))
-                    if TaskDistance < 10 then
-                        inRange = true
-                        DrawMarker(2, v.coords.x, v.coords.y, v.coords.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 0.2, 55, 155, 255, 255, 0, 0, 0, 1, 0, 0, 0)
-                        if TaskDistance < 1.5 then
-
-                            -- this is shit, I already have plans
-                            local requiredItems = {
-                                [1] = {name = QBCore.Shared.Items["screwdriverset"]["name"], image = QBCore.Shared.Items["screwdriverset"]["image"]},
-                                [2] = {name = QBCore.Shared.Items["copper"]["name"], image = QBCore.Shared.Items["copper"]["image"]},
-                            }
-                            -- end shit
-
-                            DrawText3Ds(v.coords.x, v.coords.y, v.coords.z + 0.25, '[E] Complete task')
 
                             if IsControlJustPressed(0, 38) then
-                                QBCore.Functions.TriggerCallback('qb-telco:server:HasToolkit', function(hasItem)
-                                    if hasItem then
-                                        -- Prevent sticky panel 
-                                        TriggerEvent('inventory:client:requiredItems', requiredItems, false)
-                                        BuilderData.CurrentTask = k
-                                        DoTask()
-                                    else    
-                                        TriggerEvent('inventory:client:requiredItems', requiredItems, true)
-                                    end
-                                end)
+                                BuilderData.ShowDetails = not BuilderData.ShowDetails
                             end
-                        else
-                            TriggerEvent('inventory:client:requiredItems', requiredItems, false)
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Projects[Config.CurrentProject].ProjectLocations["tasks"]) do
+                    if not v.completed or not v.IsBusy then
+                        local TaskDistance = #(pos - vector3(v.coords.x, v.coords.y, v.coords.z))
+                        if TaskDistance < 10 then
+                            inRange = true
+                            DrawMarker(2, v.coords.x, v.coords.y, v.coords.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 0.2, 55, 155, 255, 255, 0, 0, 0, 1, 0, 0, 0)
+                            if TaskDistance < 1.5 then
+
+                                -- this is shit, I already have plans
+                                local requiredItems = {
+                                    [1] = {name = QBCore.Shared.Items["screwdriverset"]["name"], image = QBCore.Shared.Items["screwdriverset"]["image"]},
+                                    [2] = {name = QBCore.Shared.Items["copper"]["name"], image = QBCore.Shared.Items["copper"]["image"]},
+                                }
+                                -- end shit
+
+                                DrawText3Ds(v.coords.x, v.coords.y, v.coords.z + 0.25, '[E] Complete task')
+
+                                if IsControlJustPressed(0, 38) then
+                                    QBCore.Functions.TriggerCallback('qb-telco:server:HasToolkit', function(hasItem)
+                                        if hasItem then
+                                            -- Prevent sticky panel 
+                                            TriggerEvent('inventory:client:requiredItems', requiredItems, false)
+                                            BuilderData.CurrentTask = k
+                                            DoTask()
+                                        else    
+                                            TriggerEvent('inventory:client:requiredItems', requiredItems, true)
+                                        end
+                                    end)
+                                end
+                            else
+                                TriggerEvent('inventory:client:requiredItems', requiredItems, false)
+                            end
                         end
                     end
                 end
             end
+        else
+            Citizen.Wait(1000)
         end
-
         if not inRange then
             Citizen.Wait(1000)
         end
@@ -226,7 +241,7 @@ RegisterNetEvent('qb-telco:client:UpdateBlip')
 AddEventHandler('qb-telco:client:UpdateBlip', function(id)
     DeleteBlip()
     Citizen.Wait(5)
---  if PlayerJob.name == "tow" then
+    if PlayerJob.name == "telco" then
     TelcoBlip = AddBlipForCoord(Config.Projects[id].ProjectLocations["main"].coords.x, Config.Projects[id].ProjectLocations["main"].coords.y, Config.Projects[id].ProjectLocations["main"].coords.z)
     SetBlipSprite(TelcoBlip, 354)
     SetBlipDisplay(TelcoBlip, 4)
@@ -236,7 +251,7 @@ AddEventHandler('qb-telco:client:UpdateBlip', function(id)
     BeginTextCommandSetBlipName("STRING")
     AddTextComponentSubstringPlayerName(Config.Projects[id].ProjectLocations["main"].label)
     EndTextCommandSetBlipName(TelcoBlip)
---  end
+    end
 
 end)
 
