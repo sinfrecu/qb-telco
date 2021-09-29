258f5a415728fc92c7ff15373332aeaf863762c4
matias <matias@matiasba.com.ar>
2021-09-29 03:34:33 +0000

    sarlanga

--- client/main.lua
+++ client/main.lua
@@ -396,7 +396,8 @@ Citizen.CreateThread(function()
                                 -- end shit
                                 DrawText3Ds(v.coords.x, v.coords.y, v.coords.z + 0.25, '[E] '..v.label )                
                                 if IsControlJustPressed(0, 38) then
-                                    local sarlanga.quien = k                                  
+                                    local  sarlanga {}
+                                    sarlanga.quien = k                                  
                                     -- TriggerServerEvent('qb-telco:server:CurrenTaskupdate', k )
                                     QBCore.Functions.TriggerCallback('qb-telco:server:HasToolkit', function(hasItem)
                                         if hasItem then
